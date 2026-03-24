f9a287f98d2478f966a69dd2992414918d6a0de2
Gahben <158853568+Gahben@users.noreply.github.com>
2026-03-24 14:02:57 -0300

    update simples

--- server.cjs
+++ server.cjs
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'dist'), {
 }));
 
 // SPA fallback
-app.get('(.*)', (req, res) => {
+app.get('/:splat*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
