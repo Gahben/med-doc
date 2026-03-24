d9d4b3d1d020af4f9a48742f2d1e41545a47b10f
Gahben <158853568+Gahben@users.noreply.github.com>
2026-03-24 11:44:19 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    server: {
-      port: 3000,
-    },
-    build: {
-      outDir: 'dist',
-    },
-    base: './', // <--- Adicione isso para usar caminhos relativos
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  base: './'
 })
