790451c566f7bc062ee1463a3843dc661e42e6d9
Gahben <158853568+Gahben@users.noreply.github.com>
2026-05-10 10:29:49 -0300

    update

--- supabase/functions/cron-clean-trash/index.ts
+++ supabase/functions/cron-clean-trash/index.ts
@@ -1,5 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
+declare const Deno: any;
+
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
